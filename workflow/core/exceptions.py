716d426f23ef1e19b85f8f96d5324d472db23205
Justin Ovens <jovens@gotunixcode.net>
2022-07-17 20:54:37 -0400

    Added RunCommandsException and MessagesException to workflow/core/exceptions.py

--- workflow/core/exceptions.py
+++ workflow/core/exceptions.py
@@ -30,3 +30,11 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
+
+
+class RunCommandsException(Exception):
+    pass
+
+
+class MessagesException(Exception):
+    pass
