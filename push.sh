9f290866519c8c49b1d016338d5ff55fc4badff1
Justin Ovens <jovens@gotunixcode.net>
2022-07-17 20:53:28 -0400

    Added Colors class to workflow/core/common.py

--- push.sh
+++ push.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 DEST="git@github.com:gotunixcode/workflow.git"
-BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 
-if [[ "${BRANCH}" != "main" ]]; then
-    echo "Current branch: ${BRANCH}"
-    echo "You can only mirror the main branch"
-    exit 1
+echo "Pushing repo to ${DEST}"
+git push --mirror ${DEST}
+if [ $? -eq 0 ]; then
+    echo "Completed successfully"
+    exit 0
 else
-    echo "Pushing repo to ${DEST}"
-    git push --mirror ${DEST}
-    if [ $? -eq 0 ]; then
-        echo "Completed successfully"
-        exit 0
-    else
-        echo "Failure to push to ${DEST}"
-        exit 1
-    fi
+    echo "Failure to push to ${DEST}"
+    exit 1
 fi
