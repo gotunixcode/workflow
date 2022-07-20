55b9f76827e79cb64e687c928eaaf9d2ba7efebb
Justin Ovens <jovens@gotunixcode.net>
2022-07-20 19:21:31 -0400

    working on backup and update class

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -30,3 +30,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
+
+
+VERSION="v0.1.0"
